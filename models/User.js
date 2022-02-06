b6eef32e8750f633a1e150c2ec64f6741dfe9324
yusufstawan <yusufsetiyawan45@gmail.com>
2022-02-06 15:53:53 +0700

    update lib path

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema(
         },
         password: {
             type: String,
-            requred: true
+            required: true
         },
         profilePic: {
             type: String,
